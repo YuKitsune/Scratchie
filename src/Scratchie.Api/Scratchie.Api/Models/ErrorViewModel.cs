cbd8528eebe760563414fb15237a6d117710039a
Eoin Motherway <eoinmoth@yahoo.ie>
2020-11-22 16:30:12 +1000

    Initial commit

---
+++ src/Scratchie.Api/Scratchie.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Scratchie.Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
