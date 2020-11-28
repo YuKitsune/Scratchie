b0194ec872af599f731f2a5f694ab7439299dda7
Eoin Motherway <eoinmoth@yahoo.ie>
2020-11-28 16:55:30 +1000

    Added hide/unhide via key binding

--- src/macos/Scratchie/Scratchie/ScratchieApp.swift
+++ src/macos/Scratchie/Scratchie/ScratchieApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import HotKey
 
 @main
 struct ScratchieApp: App {
+    
+    var toggleVisibilityHotKey: HotKey
+    
     var body: some Scene {
         WindowGroup {
             ContentView().environmentObject(UserData())
+        }
+    }
+    
+    init() {
+        toggleVisibilityHotKey = HotKey(key: .r, modifiers: [.command, .option])
+        toggleVisibilityHotKey.keyDownHandler = toggleVisability
+    }
+    
+    func toggleVisability() {
+        if NSApp.isHidden {
+            NSApp.unhide(nil)
+            NSApp.activate(ignoringOtherApps: true)
+        } else {
+            NSApp.hide(nil)
         }
     }
 }
