23e021dbf1bc87b417be706582cfa44061d95c20
Eoin Motherway <eoinmoth@yahoo.ie>
2020-12-22 23:23:07 +1000

    Tidy-up

--- src/macos/Scratchie/Scratchie/ContentView.swift
+++ src/macos/Scratchie/Scratchie/ContentView.swift
@@ -10,9 +10,10 @@ import SwiftUI
 struct ContentView: View {
     @EnvironmentObject private var userData: UserData
     
+    // BUG: When the view is closed, it cannot be re-opened, may need to consider re-instanciating the view from the ScratchieApp file if it's been closed, or somehow overriding the close command
     var body: some View {
         ScratchpadEditor(scratchpad: userData.scratchpad)
-            .onAppear(perform: ScratchieApp.configureStatusBarButton)            
+            .onAppear(perform: ScratchieApp.configureStatusBarButton)
     }
 }
 
