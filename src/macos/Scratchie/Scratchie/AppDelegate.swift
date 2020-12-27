8d8c5a9a47ac9df9bbbed2c9f5f3f7383adc8600
Eoin Motherway <eoinmoth@yahoo.ie>
2020-12-27 18:04:47 +1000

    AppDelegate cleanup

--- src/macos/Scratchie/Scratchie/AppDelegate.swift
+++ src/macos/Scratchie/Scratchie/AppDelegate.swift
@@ -12,17 +12,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     var popover = NSPopover.init()
     var statusBarItem: NSStatusItem?
-    let modifiers: NSEvent.ModifierFlags = [NSEvent.ModifierFlags.command, NSEvent.ModifierFlags.option]
     var toggleVisibilityHotKey: HotKey?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
             
         // Create the show/hide hotkey
-        toggleVisibilityHotKey = HotKey(key: .r, modifiers: modifiers)
+        toggleVisibilityHotKey = HotKey(key: .r, modifiers: [.command, .option])
         toggleVisibilityHotKey!.keyDownHandler = togglePopover
         
         // Create the Content View
-        let contentView = ContentView().environmentObject(UserData()).frame(width: 600, height: 600, alignment: .center)
+        let contentView = ContentView()
+            .environmentObject(UserData())
+            .frame(width: 600, height: 600, alignment: .center)
 
         // Set the SwiftUI's ContentView to the Popover's ContentViewController
         // Todo: Find out how to allow the user to resize the popover
@@ -34,29 +35,25 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.squareLength))
         self.statusBarItem?.button?.title = "Scratchie"
         self.statusBarItem?.button?.image = NSImage(named: "MenuBarIcon")
-        self.statusBarItem?.button?.action = #selector(AppDelegate.togglePopover(_:))
+        self.statusBarItem?.button?.action = #selector(AppDelegate.togglePopover)
     }
     
-    @objc func showPopover(_ sender: AnyObject?) {
+    @objc func togglePopover() {
+        if popover.isShown {
+            closePopover()
+        } else {
+            showPopover()
+        }
+    }
+    
+    @objc func showPopover() {
         if let button = statusBarItem?.button {
             popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
 //            !!! - displays the popover window with an offset in x in macOS BigSur.
         }
     }
     
-    @objc func closePopover(_ sender: AnyObject?) {
-        popover.performClose(sender)
-    }
-    
-    @objc func togglePopover(_ sender: AnyObject?) {
-      togglePopover()
-    }
-    
-    func togglePopover() {
-        if popover.isShown {
-            closePopover(nil)
-        } else {
-            showPopover(nil)
-        }
+    @objc func closePopover() {
+        popover.performClose(nil)
     }
 }
