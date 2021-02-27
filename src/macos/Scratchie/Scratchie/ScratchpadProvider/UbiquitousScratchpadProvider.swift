c5293148736e87da0af98eb8d77586fe4d8dc697
Eoin Motherway <eoin@yukitsune.dev>
2021-02-27 14:48:21 +1000

    Rename iCloudScratchpadProvider to UbiquitousScratchpadProvider
    Also remove keyStore variable in favour of NSUbiquitousKeyValueStore.default

--- src/macos/Scratchie/Scratchie/ScratchpadProvider/UbiquitousScratchpadProvider.swift
+++ src/macos/Scratchie/Scratchie/ScratchpadProvider/UbiquitousScratchpadProvider.swift
@@ -1,5 +1,5 @@
 //
-//  iCloudScratchpadProvider.swift
+//  UbiquitousScratchpadProvider.swift
 //  Scratchie
 //
 //  Created by Eoin Motherway on 22/2/21.
@@ -7,26 +7,25 @@
 
 import Foundation
 
-class iCloudScratchpadProvider : ScratchpadProvider {
+class UbiquitousScratchpadProvider : ScratchpadProvider {
     
-    var key = "scratchpad"
-    var keyStore = NSUbiquitousKeyValueStore()
+    var key = "dev.YuKitsune.Scratchie.scratchpad"
     private var onExternalChangeCallback: (() -> Void)?
     
     init() {
         NotificationCenter.default.addObserver(
             self,
-            selector: #selector(onUbiquitousKeyValueStoreDidChangeExternally(notification:)),
+            selector: #selector(onUbiquitousKeyValueStoreDidChangeExternally),
             name: NSUbiquitousKeyValueStore.didChangeExternallyNotification,
             object: NSUbiquitousKeyValueStore.default)
     }
     
     func getScratchpadContent() -> String {
-        keyStore.string(forKey: key) ?? ""
+        NSUbiquitousKeyValueStore.default.string(forKey: key) ?? ""
     }
     
     func setScratchpadContent(_ content: String) {
-        return keyStore.set(content, forKey: key)
+        return NSUbiquitousKeyValueStore.default.set(content, forKey: key)
     }
     
     func onExternalChange(do callback: @escaping () -> Void) {
@@ -34,7 +33,7 @@ class iCloudScratchpadProvider : ScratchpadProvider {
     }
     
     func flush() {
-        keyStore.synchronize()
+        NSUbiquitousKeyValueStore.default.synchronize()
     }
     
     @objc private func onUbiquitousKeyValueStoreDidChangeExternally(notification: Notification) {
